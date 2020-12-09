4d3de71fff4f669a63da91774f2c634aa03dd1ca
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 00:16:57 +0530

    refactor: delete refresh tokens

--- server/src/sendRefreshToken.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from "express";
-
-export const sendRefreshToken = (res: Response, token: string) => {
-  res.cookie("jid", token, {
-    httpOnly: true,
-    path: "/refresh_token"
-  });
-};
