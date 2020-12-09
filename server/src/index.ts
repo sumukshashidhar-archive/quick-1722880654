7182cd10a01915a249a5fa3cc5794f4e07663db6
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 00:15:50 +0530

    feat: remove refresh tokens

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,54 +6,15 @@ import { buildSchema } from "type-graphql";
 import { UserResolver } from "./UserResolver";
 import { createConnection } from "typeorm";
 import cookieParser from "cookie-parser";
-import { verify } from "jsonwebtoken";
 import cors from "cors";
-import { User } from "./entity/User";
-import { sendRefreshToken } from "./sendRefreshToken";
-import { createAccessToken, createRefreshToken } from "./auth";
 import { PostResolver } from "./PostResolver";
 
 (async () => {
   console.log("Running code")
   const app = express();
-  app.use(
-    cors({
-      origin: "http://localhost:3000",
-      credentials: true
-    })
-  );
+  app.use(cors());
   app.use(cookieParser());
-  app.get("/", (_req, res) => res.send("hello"));
-  app.post("/refresh_token", async (req, res) => {
-    const token = req.cookies.jid;
-    if (!token) {
-      return res.send({ ok: false, accessToken: "" });
-    }
-
-    let payload: any = null;
-    try {
-      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
-    } catch (err) {
-      console.log(err);
-      return res.send({ ok: false, accessToken: "" });
-    }
-
-    // token is valid and
-    // we can send back an access token
-    const user = await User.findOne({ id: payload.userId });
-
-    if (!user) {
-      return res.send({ ok: false, accessToken: "" });
-    }
-
-    if (user.tokenVersion !== payload.tokenVersion) {
-      return res.send({ ok: false, accessToken: "" });
-    }
-
-    sendRefreshToken(res, createRefreshToken(user));
-
-    return res.send({ ok: true, accessToken: createAccessToken(user) });
-  });
+  app.get("/", (_req, res) => res.send("Functional"));
 
   await createConnection();
 
