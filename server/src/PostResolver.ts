512044b6f617f65f58f29edd64b5b0d32db35038
sumukshashidhar <sumukuuu@gmail.com>
2020-12-09 21:42:51 +0530

    feat: Add makePosts

--- server/src/PostResolver.ts
+++ server/src/PostResolver.ts
@@ -1,6 +1,54 @@
-import {Resolver} from "type-graphql";
+import {Query, Resolver, UseMiddleware, Ctx, Mutation, Arg} from "type-graphql";
+import { MyContext } from "./MyContext";
+import { User } from "./entity/User";
+import { isAuth } from "./isAuth";
+
+import {Post} from "./entity/Post";
+// @ObjectType()
+// class PostsData {
+//     @Field()
+//     posts: Array<String>
+// }
+
+function mysqlDate(){
+    const date = new Date();
+    return date.toISOString().split('T')[0];
+}
+
 
 @Resolver()
 export class PostResolver {
 
+    @Query(() => String)
+    @UseMiddleware(isAuth)
+    async getPosts(@Ctx() { payload }: MyContext) {
+        const id = payload?.userId;
+        const user = await User.findOne({ where: { id:id } });
+        return `${user!.email}`
+    }
+
+
+    @Mutation(() => Boolean)
+    @UseMiddleware(isAuth)
+    async makePost(
+        @Ctx() { payload }: MyContext,
+        @Arg("content") content: string
+    ) {
+        console.log(content)
+        console.log(payload)
+        try {
+
+            await Post.insert({
+                timestamp: mysqlDate(),
+                content: content,
+                user: await User.findOne({where: {id: payload!.userId}})
+            })
+
+            return true;
+
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
 }
