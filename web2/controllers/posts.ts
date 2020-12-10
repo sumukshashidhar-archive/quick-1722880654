80eaaacad3298b7f8af28c5af5fe7d9d88a41e21
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 19:32:38 +0530

    feat: add posts controller

---
+++ web2/controllers/posts.ts
@@ -0,0 +1,13 @@
+function prepareLinks(postids) {
+    let linkarray = [];
+    for(let i=0; i < postids.length; i++) {
+        linkarray.push(
+            "https://quickhq.tech/"postids[i]
+            )
+    }
+    return []
+}
+
+module.exports = {
+    
+}
