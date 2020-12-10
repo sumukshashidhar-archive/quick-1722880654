583c7bcc4f81d97bb096c786304ddb0cf46f36a7
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 12:05:48 +0530

    feat: add index file

--- web/quickhq/src/index.js
+++ web/quickhq/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {ApolloProvider} from "@apollo/react-hooks"
+import ApolloClient from 'apollo-boost';
+import {Routes} from './routes'
+
+const client = new ApolloClient({
+    uri: "https://backend.quickhq.tech/graphql"
+})
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ApolloProvider client={client}>
+        <Routes />
+    </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
