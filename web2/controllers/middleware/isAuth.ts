6f35eed64e7aadcd0c5b3e4241bac7961951dacc
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 20:57:50 +0530

    feat: add checker for auth

--- web2/controllers/middleware/isAuth.ts
+++ web2/controllers/middleware/isAuth.ts
@@ -1,4 +1,20 @@
-module.exports = function(req, res, next) {
-    console.log(req.cookies)
-    next()
+const jwt = require("jsonwebtoken")
+module.exports = async function(req, res, next) {
+    if (req.cookies["Authorization"]) {
+        try {
+            const data = await jwt.decode(req.cookies["Authorization"].split(' ')[1])
+            if(Date.now() >= data.exp * 1000) {
+                res.redirect('/login')
+            }
+            else {
+                next()
+            }
+        } catch (e) {
+            res.redirect('/login')
+            
+        }
+    }
+    else {
+        res.redirect('/login')
+    }
 }
