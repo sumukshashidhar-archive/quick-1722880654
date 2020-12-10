0455873ce4912aac438499eb799bfbb29e9a979e
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 21:02:41 +0530

    feat: add register redirection

--- web2/routes/index.ts
+++ web2/routes/index.ts
@@ -12,5 +12,8 @@ export default function Routes(app) {
     //dashboard
     Dashboard(app);
 
+    app.get('/register', (_, res) => {res.sendFile(path.resolve('views/register.html'))})
+
+
     app.get('/posts', (_, res) => {res.render('posts.ejs', {object: {"posts": ["1", "2", "3"], "timestamps":[4, 5, 6], "edits":["7", "8", "9"]}})})
 }
