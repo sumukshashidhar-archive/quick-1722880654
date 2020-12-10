9f070f385684ed0bd7f8e97c476bec7c0e147807
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 20:38:25 +0530

    feat: add graphql queries

---
+++ web2/graphql/queries.ts
@@ -0,0 +1,41 @@
+
+import { gql } from 'graphql-request'
+
+const LoginMutation = gql`
+mutation Login($email: String!, $password: String!) {
+
+    login(email: $email, password: $password) {
+        accessToken
+    }
+}
+`
+
+const PostMaker = gql`
+mutation makePost($content: String!) {
+
+    makePost(content: $content) {
+
+    }
+}
+`
+
+const getPostsForUser = gql`
+
+query getPosts{
+    id
+    timestamp
+    content
+}
+
+`
+
+
+
+module.exports = {
+
+
+    login: LoginMutation,
+    makePost: PostMaker,
+    getPosts: getPostsForUser
+
+}
