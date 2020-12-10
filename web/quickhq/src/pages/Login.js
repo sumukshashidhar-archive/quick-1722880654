91ca2d1b913d1915af4983043f966ff5ff00587e
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 19:08:54 +0530

    feat: modify login page

--- web/quickhq/src/pages/Login.js
+++ web/quickhq/src/pages/Login.js
@@ -32,12 +32,6 @@ export const Login = () => {
       <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
         Sign in to your account
       </h2>
-      <p className="mt-2 text-center text-sm text-gray-600">
-        Or
-        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-          start your 14-day free trial
-        </a>
-      </p>
     </div>
     <form className="mt-8 space-y-6" action="#" method="POST">
       <input type="hidden" name="remember" defaultValue="true" />
