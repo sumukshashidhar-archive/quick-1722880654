482a192f94eca271986159b34f0be6b827002bf5
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 20:58:03 +0530

    feat: add checker for no cookie

--- web2/routes/login.ts
+++ web2/routes/login.ts
@@ -1,10 +1,31 @@
 import { celebrate, Joi, Segments } from "celebrate";
 const path = require("path")
 import LoginController from "./../controllers/login"
+const jwt = require("jsonwebtoken")
+
+async function checkerToken(req, res, next) {
+    if (req.cookies["Authorization"]) {
+        try {
+            const data = await jwt.decode(req.cookies["Authorization"].split(' ')[1])
+            if(Date.now() >= data.exp * 1000) {
+                next()
+            }
+            else {
+                res.redirect('/dashboard')
+            }
+        } catch (e) {
+            next()
+        }
+    }
+    else {
+        next()
+    }
+}
+
 
 export default function Login(app) {
 
-    app.get('/login', (_, res) => {
+    app.get('/login', checkerToken, (_, res) => {
         res.sendFile(path.resolve('views/login.html'))
     })
 
