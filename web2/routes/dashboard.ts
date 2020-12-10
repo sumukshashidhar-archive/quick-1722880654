b75d9e301190b89353d3262834da797cf4e5959f
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 22:02:06 +0530

    feat: add dashboard and logout routes

--- web2/routes/dashboard.ts
+++ web2/routes/dashboard.ts
@@ -1,9 +1,29 @@
 const isAuth = require('../controllers/middleware/isAuth')
-
+const postcon = require("./../controllers/posts")
 
 export default function Dashboard(app) {
 
-    app.get('/dashboard', isAuth, (req, res) => {
-        res.send("ok")
+    app.get('/dashboard', isAuth, async (req, res) => {
+        try {
+            const data = await postcon.getPosts(req.cookies["Authorization"])
+            if (data.length == 0) {
+                res.send("no")
+            } else {
+                res.render('posts', {object: {
+                    posts: data.data,
+                    links: data.links
+                }})
+            }
+
+            console.log(data)
+        } catch (e) {
+            console.log(e)
+        }
+
+    })
+
+    app.get('/logout', async(req, res) => {
+        res.clearCookie('Authorization')
+        res.redirect('/')
     })
 }
