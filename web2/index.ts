6e97c1775729dc861d473c68caedcda43d329953
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 20:36:38 +0530

    feat: add cookieParser

--- web2/index.ts
+++ web2/index.ts
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 import Routes from "./routes/index";
 const path = require("path");
+const cookieParser = require("cookie-parser")
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
 Routes(app)
 
