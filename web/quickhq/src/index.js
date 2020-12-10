cb57a17621592a093e270a7427f72e9f7026a28d
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 19:08:35 +0530

    feat: modify react index file

--- web/quickhq/src/index.js
+++ web/quickhq/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import {ApolloProvider} from "@apollo/react-hooks"
 import ApolloClient from 'apollo-boost';
 import {Routes} from './routes'
+import App from "./App2"
 
 const client = new ApolloClient({
     uri: "https://backend.quickhq.tech/graphql"
