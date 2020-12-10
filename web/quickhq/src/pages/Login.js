970598cd3f509774c00a47dfa9dcb43281168494
sumukshashidhar <sumukuuu@gmail.com>
2020-12-10 12:07:29 +0530

    natively integrate header with login page

--- web/quickhq/src/pages/Login.js
+++ web/quickhq/src/pages/Login.js
@@ -1,9 +1,23 @@
 import React from "react";
-import {Header} from "./../header-login"
 export const Login = () => {
     return (
         <>
-        <Header />
+              <header className="text-gray-700 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+            </svg>
+            <span className="ml-3 text-xl">QuickHQ</span>
+          </a>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center"></nav>
+          <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0" onClick={handleClick}>Home
+            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7" />
+            </svg>
+          </button>
+        </div>
+      </header> 
 <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
   <div className="max-w-md w-full space-y-8">
     <div>
