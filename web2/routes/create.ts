d3e7af4cebaa4505323977e631c59158ac581218
sumukshashidhar <sumukuuu@gmail.com>
2020-12-11 20:46:14 +0530

    feat: add logic to postController

--- web2/routes/create.ts
+++ web2/routes/create.ts
@@ -1,19 +1,28 @@
 import {celebrate, Segments, Joi} from "celebrate";
 import express from "express";
+import PostController from "../controllers/createPostController";
+import path from "path";
 
 export default function CreatePost(app: express.Application) {
 
+    app.get('/create', function (_, res){
+        res.sendFile(path.resolve('views/create.html'))
+    })
 
-
-    app.get('/create', celebrate({
+    app.post('/create', celebrate({
         [Segments.BODY]: Joi.object().keys({
             content: Joi.string().required(),
         })
-    })), async (req: express.Request, res: express.Response) => {
+    }), async (req: express.Request, res: express.Response) => {
         try {
-
+            const response = await PostController(req.body.content, req.cookies.authorization)
+            if(response) {
+                res.redirect('/success')
+            } else {
+                res.redirect('/failure')
+            }
         } catch (e) {
 
         }
-    }
+    })
 }
