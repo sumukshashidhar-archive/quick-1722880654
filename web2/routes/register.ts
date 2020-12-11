6e095ae36510dcfd23689b9551ca600a6412732f
sumukshashidhar <sumukuuu@gmail.com>
2020-12-11 21:23:08 +0530

    feat: import register controller

--- web2/routes/register.ts
+++ web2/routes/register.ts
@@ -1,7 +1,9 @@
-// import { celebrate, Joi, Segments } from "celebrate";
+import { celebrate, Joi, Segments } from "celebrate";
 import express from "express";
 const path = require("path")
+import RegisterController from "../controllers/register";
 const jwt = require("jsonwebtoken")
+
 async function checkerToken(req: express.Request, res: express.Response, next:express.NextFunction) {
     if (req.cookies["Authorization"]) {
         try {
@@ -27,17 +29,21 @@ export default function Register(app: express.Application) {
         res.sendFile(path.resolve('views/register.html'))
     })
 
-    // app.post('/register', celebrate({
-    //     [Segments.BODY]: Joi.object().keys({
-    //         email: Joi.string().required(),
-    //         password: Joi.string().required(),
-    //         c_password: Joi.string().required(),
-    //     })
-    // })), async (req, res) => {
-    //     try {
-    //
-    //     } catch (e) {
-    //
-    //     }
-    // }
+    app.post('/register', celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            email: Joi.string().required(),
+            password: Joi.string().required(),
+        })
+    }), async (req, res) => {
+        try {
+            const response = await RegisterController(req.body.email, req.body.password);
+            if(response) {
+                res.redirect('/login')
+            } else {
+                res.redirect('/failure')
+            }
+        } catch (e) {
+            res.redirect('/failure')
+        }
+    })
 }
