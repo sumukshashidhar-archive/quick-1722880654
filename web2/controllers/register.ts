a72db3ebbd7b84da973c87ea609fa4293655d3f1
sumukshashidhar <sumukuuu@gmail.com>
2020-12-11 21:23:22 +0530

    feat: develop register controller

--- web2/controllers/register.ts
+++ web2/controllers/register.ts
@@ -2,19 +2,19 @@ require('dotenv').config()
 const queries = require("./../graphql/queries")
 import { GraphQLClient} from 'graphql-request'
 
-export default async function RegisterController(email, password) {
+export default async function RegisterController(email: String, password:String) {
     return new Promise(async (resolve, reject) => {
         try {
-            let loginMutation = queries.login;
+            let registerMutation = queries.register;
             let variables = {
                 email: email,
                 password: password
             }
     
             const data = await new GraphQLClient(process.env.ENDPOINT!)
-                            .request(loginMutation, variables)
+                            .request(registerMutation, variables)
 
-            resolve(data.login.accessToken)
+            resolve(data.register)
         } catch (err) {
             console.log(err)
             reject(err)
