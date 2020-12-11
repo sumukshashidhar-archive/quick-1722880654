259ba960049a9792545f437f4836bbaed4332ab9
sumukshashidhar <sumukuuu@gmail.com>
2020-12-11 20:44:44 +0530

    feat: add typing

--- web2/controllers/login.ts
+++ web2/controllers/login.ts
@@ -2,7 +2,7 @@ require('dotenv').config()
 const queries = require("./../graphql/queries")
 import { GraphQLClient } from 'graphql-request'
 
-export default async function LoginController(email, password) {
+export default async function LoginController(email: String, password: String) {
     return new Promise(async (resolve, reject) => {
         try {
             let loginMutation = queries.login;
